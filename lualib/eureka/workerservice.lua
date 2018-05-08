4b13a9d48b0623e27b9d99e10e6483664e1d2109
xiaooloong <xiaooloong@126.com>
2018-05-08 14:47:31 +0800

    use module cache per worker

--- lualib/eureka/workerservice.lua
+++ lualib/eureka/workerservice.lua
@@ -2,25 +2,36 @@ local client = require 'eureka.client'
 
 local _register, _renew
 
-_register = function(premature, instance)
+local eurekaclient, eurekaserver, instance, timeval
+
+_register = function(premature)
     if premature then
         return
     end
-    return eurekaclient:register(instance.app, {
-        instance = instance,
-    })
-
+    local err
+    eurekaclient, err = client:new(eurekaserver.host, eurekaserver.port, eurekaserver.uri)
+    if not eurekaclient then
+        ngx.log(ngx.ALERT, ('can not create client instance %s : %s'):format(instance.instanceId, err))
+    else
+        local ok, err = eurekaclient:register(instance.app, {
+            instance = instance,
+        })
+        if not ok then
+            ngx.log(ngx.ALERT, ('can not register instance %s : %s'):format(instance.instanceId, err))
+        end
+    end
 end
 
-_renew = function(premature, eurekaclient, instance, timeval)
+_renew = function(premature)
     if premature then
         return eurekaclient:deRegister(instance.app, instance.instanceId)
     end
+    local eurekaclient = eurekaclient
     local ok, err = eurekaclient:heartBeat(instance.app, instance.instanceId)
     if not ok or ngx.null == ok then
         ngx.log(ngx.ALERT, ('failed to renew instance %s : %s'):format(instance.instanceId, err))
     else
-        ngx.timer.at(timeval, _renew, eurekaclient, instance, timeval)
+        ngx.timer.at(timeval, _renew)
     end
 end
 
@@ -28,8 +39,11 @@ local _M = {
     ['_VERSION'] = '0.0.1'
 }
 
-function _M.run(self, eurekaserver, instance)
-    local timeval = tonumber(eurekaserver.timeval) or 5
+function _M.run(self, _eurekaserver, _instance)
+    instance = _instance
+    eurekaserver = _eurekaserver
+    timeval = tonumber(eurekaserver.timeval) or 5
+
     instance.metadata.language = 'ngx_lua'
     instance.dataCenterinfo = {
         name = 'MyOwn',
@@ -37,15 +51,8 @@ function _M.run(self, eurekaserver, instance)
     }
     instance.instanceId = ('%s:%s:%d'):format(instance.ipAddr, instance.app, ngx.worker.pid())
 
-    local eurekaclient = client:new(eurekaserver.host, eurekaserver.port, eurekaserver.uri)
-
-    ngx.timer.at(0, _register, instance)
-
-    if ok then
-        ngx.timer.at(timeval, _renew, eurekaclient, instance, timeval)
-    else
-        ngx.log(ngx.ALERT, ('failed to start eureka service at %s : %s'):format(instance.instanceId, err))
-    end
+    ngx.timer.at(0, _register)
+    ngx.timer.at(timeval, _renew)
 end
 
 return _M
